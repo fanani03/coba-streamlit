0572c96f88e0e411ed4dcb10d322d55b45fbc872
fanani03 <afanfanani202@gmail.com>
2022-11-11 14:34:24 +0700

    fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,12 +9,10 @@ st.header("Ini Header - Belajar KNN")
 url = "https://vincentarelbundock.github.io/Rdatasets/csv/Stat2Data/Cuckoo.csv"
 df = pd.read_csv(url)
 
-st.write("Menampilkan informasi tipe data di semua kolom")
-st.write(df.info())
 
+df.info()
 
-st.write("Menampilkan 5 baris teratas")
-st.write(df.head(10))
+df.head(10)
 
 #Mean
 st.write("Mean nya adalah ",df["Length"].mean())
@@ -25,7 +23,3 @@ st.write("Median nya adalah ",df["Length"].median())
 #Modus
 st.write("Modusnya ",df["Length"].mode())
 
-
-plt.figure()
-sns.distplot(df["Length"])
-plt.show()
