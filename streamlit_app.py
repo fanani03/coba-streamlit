513ca7c476059eb6034f02824be7111889c29759
fanani03 <afanfanani202@gmail.com>
2022-11-11 14:31:43 +0700

    menambahkan keterangan 2

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,23 @@
 import streamlit as st
 import pandas as pd
 
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+st.title("Belajar KNN")
+st.header("Ini Header - Belajar KNN")
+
 #Pengukuran Burung
 url = "https://vincentarelbundock.github.io/Rdatasets/csv/Stat2Data/Cuckoo.csv"
 df = pd.read_csv(url)
 
-#Menampilkan informasi tipe data di semua kolom
-df.info()
+st.write("Menampilkan informasi tipe data di semua kolom")
+st.write(df.info())
 
-# Menampilkan 5 baris teratas
-df.head(10)
+
+st.write("Menampilkan 5 baris teratas")
+st.write(df.head(10))
 
 #Mean
 st.write("Mean nya adalah ",df["Length"].mean())
@@ -19,3 +27,8 @@ st.write("Median nya adalah ",df["Length"].median())
 
 #Modus
 st.write("Modusnya ",df["Length"].mode())
+
+
+plt.figure()
+sns.distplot(df["Length"])
+plt.show()
